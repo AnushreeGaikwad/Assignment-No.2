6177cc883bc7a3eefda28679f06d2f0bcc7606dd
AnushreeGaikwad <anushreegaikwad27@gmail.com>
2024-03-25 07:49:23 -0400

    Q.1

---
+++ Q1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+int main()
+{
+
+int one, two, div;
+
+printf("Enter first number - ");
+
+scanf("%d",&one);
+
+printf("Enter second number - ");
+
+scanf("%d",&two);
+
+div = one / two;
+
+printf("The divison of numbers %d and %d is %d",one,two,div);
+
+return 0;
+
+}
